0db1aa906753ebe01b1334c67dabce5c21cc1c35
samlsmith05 <samlsmith05@optonline.net>
2020-10-08 16:11:20 -0400

    Sam Smith: Added the recyclerView

--- app/src/main/java/com/example/libraryreservationapp/AdminHomeActivity.java
+++ app/src/main/java/com/example/libraryreservationapp/AdminHomeActivity.java
@@ -1,19 +1,31 @@
 package com.example.libraryreservationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class AdminHomeActivity extends AppCompatActivity {
-    Button btnLogout;
-    Button btnAddRoom;
-    FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    //private member variables
+    private Button btnLogout;
+    private Button btnAddRoom;
+
+    private RecyclerView recyclerView;
+    private RoomAdapter adapter;
+    private FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,14 +34,20 @@ public class AdminHomeActivity extends AppCompatActivity {
 
         btnLogout = findViewById(R.id.logout);
         btnAddRoom = findViewById(R.id.btnGoToAddRoom);
+        fStore = FirebaseFirestore.getInstance();
 
+        //calls the recycler view for it to be set up
+        setUpRecyclerView();
+
+        // Logout On Click Listener
         btnLogout.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //signs out user
                 FirebaseAuth.getInstance().signOut();
+                //Starts LoginActivity if this button is clicked
                 Intent intToLogin = new Intent(AdminHomeActivity.this, LoginActivity.class);
                 startActivity(intToLogin);
-                //Erick
             }
         }));
 
@@ -42,5 +60,30 @@ public class AdminHomeActivity extends AppCompatActivity {
                 startActivity(intToAddRoom);
             }
         });
+
+
+    }
+
+    private void setUpRecyclerView() {
+        // creates a query that uses the collection reference to get the buildings in ascending order
+        Query query = fStore.collection("rooms").orderBy("building", Query.Direction.ASCENDING).orderBy("roomNumber", Query.Direction.ASCENDING);
+
+        // creates configuratios for the adapter and binds the query to the recyclerView
+        // .setLifecycleOwner(this) allows for deletion of onStart and onStop overrides
+        FirestoreRecyclerOptions<Room> options = new FirestoreRecyclerOptions.Builder<Room>().setQuery(query, Room.class).setLifecycleOwner(this).build();
+
+        // sets the adapter with the configurations that were just made
+        adapter = new RoomAdapter(options);
+
+        // gets the recyclerView id for reference
+        recyclerView = findViewById(R.id.recyclerViewRooms);
+        // sets the layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        //adds horizontal line between different items
+        recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL));
+        // sets the adapter
+        recyclerView.setAdapter(adapter);
     }
 }
