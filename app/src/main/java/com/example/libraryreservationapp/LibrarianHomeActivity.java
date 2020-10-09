49d2677229122bb769960be2137e1899cd2aef73
Erick <erickg2u@gmail.com>
2020-10-09 11:34:03 -0400

    librarian add book is added.

--- app/src/main/java/com/example/libraryreservationapp/LibrarianHomeActivity.java
+++ app/src/main/java/com/example/libraryreservationapp/LibrarianHomeActivity.java
@@ -9,19 +9,24 @@ import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LibrarianHomeActivity extends AppCompatActivity {
-    Button btnLogout;
-
+public class LibrarianHomeActivity extends AppCompatActivity
+{
+    private Button btnLogout;
+    private Button btnAddBook;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_librarian_home);
 
         btnLogout = findViewById(R.id.logout);
+        btnAddBook = findViewById(R.id.btnGoToAddBook);
 
-        btnLogout.setOnClickListener((new View.OnClickListener() {
+        btnLogout.setOnClickListener((new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 FirebaseAuth.getInstance().signOut();
                 Intent intToLogin = new Intent(LibrarianHomeActivity.this, LoginActivity.class);
                 startActivity(intToLogin);
@@ -29,5 +34,16 @@ public class LibrarianHomeActivity extends AppCompatActivity {
             }
         }));
 
+        // Add Book On Click Listener
+        btnAddBook.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //Starts AddBookActivity if the button is clicked
+                Intent intToAddBook = new Intent(LibrarianHomeActivity.this, AddBookActivity.class);
+                startActivity(intToAddBook);
+            }
+        });
     }
 }
