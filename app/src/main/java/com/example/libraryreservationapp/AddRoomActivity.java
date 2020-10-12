01968803a19a10121a30a125d34a4db3834f08cf
samlsmith05 <samlsmith05@optonline.net>
2020-10-11 22:30:40 -0400

    Sam Smith: added the info for the available piece of data and added comments

--- app/src/main/java/com/example/libraryreservationapp/AddRoomActivity.java
+++ app/src/main/java/com/example/libraryreservationapp/AddRoomActivity.java
@@ -25,6 +25,7 @@ import java.util.Map;
 
 public class AddRoomActivity extends AppCompatActivity {
 
+    //private member variables
     private SeekBar capacitySeekBar;
     private Button addRoomButton;
     private EditText roomNumberEditText;
@@ -40,6 +41,7 @@ public class AddRoomActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_room);
 
+        //gets the references from the layout for the variables
         fStore = FirebaseFirestore.getInstance();
 
         capacitySeekBar = findViewById(R.id.seekBarCapacity);
@@ -51,96 +53,119 @@ public class AddRoomActivity extends AppCompatActivity {
         whiteboardSwitch = findViewById(R.id.switchWhiteboard);
         whitmanRadioButton = findViewById(R.id.radioButtonWhitman);
 
+        //Add Room on click listener
         addRoomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int flags = 0;
-                int roomNumber = 0;
-                String building = "";
-
-                roomNumberEditText.setError(null);
-                whitmanRadioButton.setError(null);
-
-                int capacity = capacitySeekBar.getProgress();
-                String sRoomNumber = roomNumberEditText.getText().toString();
-                if(sRoomNumber.equals("")){
-                    flags++;
-                    roomNumberEditText.setError("Please enter a numeric room number");
-                }
-                else{
-                    roomNumber = Integer.parseInt(sRoomNumber);
-                }
-
-                boolean computer = computerSwitch.isChecked();
-                boolean wifi = wifiSwitch.isChecked();
-                boolean whiteboard = whiteboardSwitch.isChecked();
-
-                int selectedBuilding = buildingRadioGroup.getCheckedRadioButtonId();
-                if(selectedBuilding == -1){
-                    flags++;
-                    whitmanRadioButton.setError("Please select a building");
-                }
-                else{
-                    switch (selectedBuilding){
-                        case R.id.radioButtonBusiness:
-                            building = getString(R.string.business);
-                            break;
-                        case R.id.radioButtonCampusCenter:
-                            building = getString(R.string.campus_center);
-                            break;
-                        case R.id.radioButtonConklin:
-                            building = getString(R.string.conklin);
-                            break;
-                        case R.id.radioButtonGleeson:
-                            building = getString(R.string.gleeson);
-                            break;
-                        case R.id.radioButtonGreenley:
-                            building = getString(R.string.greenley);
-                            break;
-                        case R.id.radioButtonHale:
-                            building = getString(R.string.hale);
-                            break;
-                        case R.id.radioButtonLupton:
-                            building = getString(R.string.lupton);
-                            break;
-                        case R.id.radioButtonSinclair:
-                            building = getString(R.string.sinclair);
-                            break;
-                        case R.id.radioButtonThompson:
-                            building = getString(R.string.thompson);
-                            break;
-                        case R.id.radioButtonWhitman:
-                            building = getString(R.string.whitman);
-                            break;
-                    }
-                }
-
-
-                if(flags == 0){
-                    Map<String, Object> roomInfo = new HashMap<>();
-                    roomInfo.put("capacity", capacity);
-                    roomInfo.put("computer", computer);
-                    roomInfo.put("wifi", wifi);
-                    roomInfo.put("whiteboard", whiteboard);
-                    roomInfo.put("building", building);
-                    roomInfo.put("roomNumber", roomNumber);
-
-                    fStore.collection("room").add(roomInfo).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                        @Override
-                        public void onComplete(@NonNull Task<DocumentReference> task) {
-                            if(task.isSuccessful()){
-                                Toast.makeText(getApplicationContext(), "Add was successful!", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-                                Toast.makeText(getApplicationContext(), "Add failed!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-
-                    finish();
-                }
-
+                //calls to gather the data
+                gatherData();
             }
         });
+    }
+
+    //gathers data from the form to be put into database
+    public void gatherData(){
+        int flags = 0;
+        int roomNumber = 0;
+        String building = "";
+
+        //clears the errors to ensure there is no false error
+        roomNumberEditText.setError(null);
+        whitmanRadioButton.setError(null);
+
+        //gets the capacity value
+        int capacity = capacitySeekBar.getProgress();
+
+        //gets the room number from the edit text
+        String sRoomNumber = roomNumberEditText.getText().toString();
+        //checks to see if it is an empty edit text
+        if(sRoomNumber.equals("")){
+            //adds a flag and an error message
+            flags++;
+            roomNumberEditText.setError("Please enter a numeric room number");
+        }
+        else{
+            //converts the string to an integer
+            roomNumber = Integer.parseInt(sRoomNumber);
+        }
+
+        //gets the values of the switches
+        boolean computer = computerSwitch.isChecked();
+        boolean wifi = wifiSwitch.isChecked();
+        boolean whiteboard = whiteboardSwitch.isChecked();
+        boolean available = true;
+
+        //gets the selected radiobutton
+        int selectedBuilding = buildingRadioGroup.getCheckedRadioButtonId();
+        //checks to make sure a radio button was selected
+        if(selectedBuilding == -1){
+            //adds a flag and an error
+            flags++;
+            whitmanRadioButton.setError("Please select a building");
+        }
+        else{
+            //switches the selected building id to the proper string of the building
+            switch (selectedBuilding){
+                case R.id.radioButtonBusiness:
+                    building = getString(R.string.business);
+                    break;
+                case R.id.radioButtonCampusCenter:
+                    building = getString(R.string.campus_center);
+                    break;
+                case R.id.radioButtonConklin:
+                    building = getString(R.string.conklin);
+                    break;
+                case R.id.radioButtonGleeson:
+                    building = getString(R.string.gleeson);
+                    break;
+                case R.id.radioButtonGreenley:
+                    building = getString(R.string.greenley);
+                    break;
+                case R.id.radioButtonHale:
+                    building = getString(R.string.hale);
+                    break;
+                case R.id.radioButtonLupton:
+                    building = getString(R.string.lupton);
+                    break;
+                case R.id.radioButtonSinclair:
+                    building = getString(R.string.sinclair);
+                    break;
+                case R.id.radioButtonThompson:
+                    building = getString(R.string.thompson);
+                    break;
+                case R.id.radioButtonWhitman:
+                    building = getString(R.string.whitman);
+                    break;
+            }
+        }
+
+        //checks to see if there are any errors
+        if(flags == 0){
+            //creates a hashmap to store all the data for the room
+            Map<String, Object> roomInfo = new HashMap<>();
+            roomInfo.put("capacity", capacity);
+            roomInfo.put("computer", computer);
+            roomInfo.put("wifi", wifi);
+            roomInfo.put("whiteboard", whiteboard);
+            roomInfo.put("building", building);
+            roomInfo.put("roomNumber", roomNumber);
+            roomInfo.put("available", available);
+
+            //adds to the database the new room with the hashmap
+            fStore.collection("room").add(roomInfo).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentReference> task) {
+                    if(task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(), "Add was successful!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(getApplicationContext(), "Add failed!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            //closes the activity
+            finish();
+        }
     }
 }
