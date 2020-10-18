abad51f3ab5cc1d8b59407717727310769020ac8
Erick <erickg2u@gmail.com>
2020-10-18 03:28:39 -0400

    Erick G: Created a RecyclerView for Request, updated the strings to accomadate.

--- app/src/main/java/com/example/libraryreservationapp/LibrarianHomeActivity.java
+++ app/src/main/java/com/example/libraryreservationapp/LibrarianHomeActivity.java
@@ -21,6 +21,7 @@ public class LibrarianHomeActivity extends AppCompatActivity
 {
     private Button btnLogout;
     private Button btnAddBook;
+    private Button btnRequest;
 
 
     private RecyclerView recyclerView;
@@ -36,6 +37,7 @@ public class LibrarianHomeActivity extends AppCompatActivity
 
         btnLogout = findViewById(R.id.logout);
         btnAddBook = findViewById(R.id.btnGoToAddBook);
+        btnRequest = findViewById(R.id.btnGoToSeeRequest);
 
 
 
@@ -70,6 +72,17 @@ public class LibrarianHomeActivity extends AppCompatActivity
             }
         });
 
+        // Request from professor On Click Listener
+        btnRequest.setOnClickListener(new View.OnClickListener()
+        {
+        @Override
+        public void onClick(View view)
+        {
+            //Starts RequestActivity if the button is clicked
+            Intent intToRequest = new Intent(LibrarianHomeActivity.this, SeeRequestActivity.class);
+            startActivity(intToRequest);
+        }
+        });
         //listens on the book adapter
         adapter.setOnItemClickListener(new BookAdapter.BookAdapterListener() {
             @Override
