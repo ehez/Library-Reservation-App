2da2faee6592094e976a62bb35b5f6557931dcae
ErickH <ehhernandezh@gmail.com>
2020-10-20 11:41:40 -0400

    Erick: Some student reserve room functionality

---
+++ app/src/main/java/com/example/libraryreservationapp/Step3Fragment.java
@@ -0,0 +1,33 @@
+package com.example.libraryreservationapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Step3Fragment extends Fragment {
+
+    static Step3Fragment instance;
+
+    public static Step3Fragment getInstance(){
+        if (instance == null)
+            instance = new Step3Fragment();
+        return instance;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_step3, container, false);
+    }
+}
